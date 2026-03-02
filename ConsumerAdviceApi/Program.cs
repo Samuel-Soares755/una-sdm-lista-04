bf05ea441af68a068249bb78431415c6ba3e66e5
Samuel-Soares755 <samu123soares@gmail.com>
2026-03-01 22:26:22 -0300

    Primeiro Commit

---
+++ ConsumerAdviceApi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "https://api.adviceslip.com/advice";
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                using (JsonDocument doc = JsonDocument.Parse(json))
+                {
+                    string conselho = doc.RootElement
+                                          .GetProperty("slip")
+                                          .GetProperty("advice")
+                                          .GetString();
+
+                    Console.WriteLine("Conselho de Hoje:");
+                    Console.WriteLine(conselho);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao consumir a API:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
